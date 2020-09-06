70ed49545fe538347cd6db2e7d6f834bbcdd6743
Rafael Tedesco <tedesco.rafaelmoutinho@gmail.com>
2020-09-06 14:54:43 -0300

    updated to Class

--- csv_files_reader.py
+++ csv_files_reader.py
@@ -8,68 +8,85 @@ from tkinter import Tk, filedialog
 import time
 
     
-root = Tk()
-root.withdraw()
-directory = filedialog.askdirectory()
-DIR = 'data'
-FILE = 'files_data.json'
 
+class csvReader():
 
-def check_exists():
-    data = {}
-    for root, dirs, files in os.walk(directory):
+    def __init__(self, DIR='data', FILE='files_data.json'):
+        self.data = {}
+        self._DIR = DIR
+        self._FILE = FILE
+        self._directory = ''
 
-        for f in files:
-            if FILE in f:
-                with open(os.path.join(root, FILE), 'r') as f:
-                    data = json.load(f)
-                    msg = f'There are {len(data)} data files in '
-                    if (len(data) == 1):
-                        msg = msg.replace('s','').replace('There are', 'There is')
-                    msg += f'{os.path.join(root, FILE)}\n'
+    def __check_exists(self):
+        data = {}
+        wn = Tk()
+        wn.withdraw()
+        self._directory = filedialog.askdirectory()
 
-                    print(msg)
-    return data
+        for root, dirs, files in os.walk(self._directory):
+
+            for f in files:
+                if self._FILE in f:
+                    with open(os.path.join(root, self._FILE), 'r') as f:
+                        self.data = json.load(f)
+                        msg = f'There are {len(data)} data files in '
+                        if (len(self.data) == 1):
+                            msg = msg.replace('s','').replace('There are', 'There is')
+                        msg += f'{os.path.join(root, self._FILE)}\n'
+
+                        print(msg)
                         
 
-def fetch_files(data):
-    new_files = 0
-    print('Fetching files...')
-    
-    for root, dirs, files in os.walk(directory):
-
-        for file in files:
-            if 'csv' in file.split()[-1]:
-                if file.lower() not in data.keys():
-                    data[file.lower().split('.')[0]] = os.path.join(root, file)
-                    print(f'WOW!!! There is a new file named {file}\n')
-                    new_files += 1
-                else:   
-                    print(f'File founded: {file} was already included\n')
-
-    return (data, new_files)
-
-
-def write_files(data, new_files):
-    if not data or new_files == 0:
-        print('Cannot found any csv file or you already have included all csv files in your data')
-
-    else:
-        print(f"Writing files...")
-        print(data)
-        time.sleep(1)
+    def __fetch_files(self):
+        new_files = 0
+        print('Fetching files...')
         
-        if not os.path.exists(DIR):
-            os.mkdir(DIR)
-            with open(os.path.join(DIR,FILE), 'w') as f:
-                json.dump(data, f, indent=4)
+        for root, dirs, files in os.walk(self._directory):
 
-        print(f'Data stored with success in {os.path.join(DIR,FILE)}!')
+            for file in files:
+                if 'csv' in file.split()[-1]:
+                    if file.lower() not in self.data.keys():
+                        self.data[file.lower().split('.')[0]] = os.path.join(root, file)
+                        print(f'WOW!!! There is a new file named {file}\n')
+                        new_files += 1
+                    else:   
+                        print(f'File founded: {file} was already included\n')
+
+        return new_files
+
+
+    def __write_files(self, new_files):
+        if not self.data or new_files == 0:
+            print('Cannot found any csv file or you already have included all csv files in your data')
+
+        else:
+            print(f"Writing {new_files} file(s)...")
+                
+            for idx, f in enumerate(self.data):
+                print(f'{idx+1} - {f}')
+                time.sleep(0.05)
+            
+            if not os.path.exists(self._DIR):
+                os.mkdir(self._DIR)
+                with open(os.path.join(self._DIR,self._FILE), 'w') as f:
+                    json.dump(self.data, f, indent=4)
+
+            print(f'Data stored with success in {os.path.join(self._DIR,self._FILE)}!')
+
+    def load_data(self):
+        """return your data file as a python dictionary to use with pandas!"""
+        f_data = filedialog.askopenfile()
+        self.data = json.load(f_data)
             
 
-data = check_exists()
-data, new_files = fetch_files(data)
-write_files(data, new_files)
+    def get_files(self):
+        self.__check_exists()
+        new_files = self.__fetch_files()
+        self.__write_files(new_files)
+
+
+    
+
 
 
 
